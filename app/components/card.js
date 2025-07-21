efdf34842cd3c5f7fd15381b24b81812f9f8da2d
Pacisho <phatdanaikaeochan@gmail.com>
2025-07-21 09:44:27 +0700

    Update card.js

--- app/components/card.js
+++ app/components/card.js
@@ -1,41 +1,65 @@
-//import ใน /app/page.js
-
 export default function Card() {
-    return (
+  return (
     <div className="container-fluid">
-        <div className="row">
-            <div className="col-md-12 text-center mb-4"></div>
-            <div className="col-md-12 text-center mb-4">
-                <h3>Our Project</h3>
-            </div>
+      <div className="row">
+        <div className="col-md-12 text-center mb-4">
+            <br></br>
+          <h3>Our Project</h3>
         </div>
-        <div className="row">
-            <div className="col-md-4 mb-4">
-                <div className="card">
-                <img src="/a5.jpg" className="card-img-top img-responsive" alt="..." />
-                <div className="card-body">
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                </div>
-                </div>
-            </div>
+      </div>
 
-            <div className="col-md-4 mb-4">
-            <div className="card">
-                <img src="/a6.jpg" className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                </div>
-                </div>
+      <div className="row">
+        {/* Card 1 */}
+        <div className="col-md-4 mb-4">
+          <div className="card h-100">
+            <img
+              src="/a10.jpg"
+              className="card-img-top w-100"
+              alt="..."
+              style={{ height: "250px", objectFit: "cover" }}
+            />
+            <div className="card-body">
+              <p className="card-text">
+               This is an example of a longer description. It is used to test whether the card can expand to fit more content without clipping or hiding the text. The card body should grow as needed to display all text properly.
+              </p>
             </div>
-            <div className="col-md-4 mb-4">
-            <div className="card">
-                <img src="/a7.jpg" className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                </div>
-                </div>
+          </div>
+        </div>
+
+        {/* Card 2 */}
+        <div className="col-md-4 mb-4">
+          <div className="card h-100">
+            <img
+              src="/a11.jpg"
+              className="card-img-top w-100"
+              alt="..."
+              style={{ height: "250px", objectFit: "cover" }}
+            />
+            <div className="card-body">
+              <p className="card-text">
+                This is an example of a longer description. It is used to test whether the card can expand to fit more content without clipping or hiding the text. The card body should grow as needed to display all text properly.
+              </p>
             </div>
+          </div>
+        </div>
+
+        {/* Card 3 */}
+        <div className="col-md-4 mb-4">
+          <div className="card h-100">
+            <img
+              src="/a12.jpg"
+              className="card-img-top w-100"
+              alt="..."
+              style={{ height: "250px", objectFit: "cover" }}
+            />
+            <div className="card-body">
+              <p className="card-text">
+                Short description just to balance the layout. This card doesn't need to expand too much, but it will match the height of others due to Bootstrap's `h-100` utility.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-    );
-  }
+  );
+}
